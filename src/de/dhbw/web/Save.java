020379949d268926424cc4e148a504a31315217f
wayoflife <dbetsche@gmail.com>
2015-05-12 11:21:10 +0200

    admin7 implementiert

--- src/de/dhbw/web/Save.java
+++ src/de/dhbw/web/Save.java
@@ -34,41 +34,57 @@ public class Save extends HttpServlet {
 			if (rq.getParameter("id") != null && !rq.getParameter("id").isEmpty()) {
 				ad.setId(Integer.parseInt(rq.getParameter("id")));
 			}
-			if (rq.getParameter("birthday") != null
-					&& !rq.getParameter("birthday").isEmpty()) {
-				try {
-					ad.setBirthday(rq.getParameter("birthday"));
-				} catch (ParseException e) {
-					// TODO ändern?
-				}
+			saveAdress(ad, rq, rs);
+			
+		} else if(rq.isUserInRole("ADMIN7")){
+			
+			String newPostcode = rq.getParameter("postcode");  
+			Address ad = new Address();
+			if (rq.getParameter("id") != null && !rq.getParameter("id").isEmpty()) {
+				ad.setId(Integer.parseInt(rq.getParameter("id")));
 			}
-			ad.setAddressform(rq.getParameter("addressform"));
-			ad.setCity(rq.getParameter("city"));
-			ad.setCountry(rq.getParameter("country"));
-			ad.setEmail(rq.getParameter("email"));
-			ad.setMobile(rq.getParameter("mobile"));
-			ad.setName(rq.getParameter("name"));
-			ad.setNumber(Integer.parseInt(rq.getParameter("number")));
-			ad.setPhone(rq.getParameter("phone"));
-			ad.setPostcode(rq.getParameter("postcode"));
-			ad.setStreet(rq.getParameter("street"));
-			ad.setVorname(rq.getParameter("vorname"));
+			if( newPostcode.startsWith("7") && ad.getPostcode().startsWith("7") ){
+				saveAdress(ad, rq, rs);
+			}
+			
+		} else {
+			rs.sendRedirect("/Adressbuch");
+		}
+	}
+
+	private void saveAdress(Address ad, HttpServletRequest rq, HttpServletResponse rs) {
+		
+		if (rq.getParameter("birthday") != null
+				&& !rq.getParameter("birthday").isEmpty()) {
 			try {
-				if (ad.save()) {
-					String referer = "/Adressbuch/Detail.jsp?id=" + ad.getId();
-					System.out.println("Speichern erfolgreich: " + referer);
-					rs.sendRedirect(referer);
-				} else {
-					String referer = rq.getHeader("Referer");
-					System.out.println("Speichern nicht erfolgreich: " + referer);
-					rs.sendRedirect(referer);
-				}
-			} catch (Exception e) {
-				// TODO: handle exception
+				ad.setBirthday(rq.getParameter("birthday"));
+			} catch (ParseException e) {
+				// TODO ändern?
 			}
 		}
-		else {
-			rs.sendRedirect("/Adressbuch");
+		ad.setAddressform(rq.getParameter("addressform"));
+		ad.setCity(rq.getParameter("city"));
+		ad.setCountry(rq.getParameter("country"));
+		ad.setEmail(rq.getParameter("email"));
+		ad.setMobile(rq.getParameter("mobile"));
+		ad.setName(rq.getParameter("name"));
+		ad.setNumber(Integer.parseInt(rq.getParameter("number")));
+		ad.setPhone(rq.getParameter("phone"));
+		ad.setPostcode(rq.getParameter("postcode"));
+		ad.setStreet(rq.getParameter("street"));
+		ad.setVorname(rq.getParameter("vorname"));
+		try {
+			if (ad.save()) {
+				String referer = "/Adressbuch/Detail.jsp?id=" + ad.getId();
+				System.out.println("Speichern erfolgreich: " + referer);
+				rs.sendRedirect(referer);
+			} else {
+				String referer = rq.getHeader("Referer");
+				System.out.println("Speichern nicht erfolgreich: " + referer);
+				rs.sendRedirect(referer);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
 	}
 }
